ca16f4f631aad9d3970654e6e0f52544274bef0f
lrtinkiv <lrtinkiv@student.jyu.fi>
2023-09-12 08:45:49 +0300

    basic operations to linked list

--- game/linkedList.h
+++ game/linkedList.h
@@ -24,5 +24,42 @@ class LinkedList  {
     ListNode<T>* tail;
     ListNode<T>* curr;
   public:
+    // Constructor
+    LinkedList() : length(0), head(nullptr), tail(nullptr), curr(nullptr) {}
+
+    // Method to insert an item at the beginning of the list
+    void insert(const T& item) {
+      ListNode<T>* newNode = new ListNode<T>(item, nullptr, head);
+
+      if (head != nullptr) {
+        head->prev = newNode;
+      }
+
+      head = newNode;
+
+      if (tail == nullptr) {
+        tail = newNode;
+      }
+
+      length++;
+    }
+
+     // Method to remove the last item from the list
+    void removeLast() {
+      if (tail != nullptr) {
+        if (tail == head) {
+          // If there's only one element in the list
+          delete tail;
+          tail = head = nullptr;
+        } else {
+          // If there are multiple elements in the list
+          tail = tail->prev;
+          delete tail->next;
+          tail->next = nullptr;
+        }
+        length--;
+      }
+    }
 
 };
+#endif // LinkedList_hpp
